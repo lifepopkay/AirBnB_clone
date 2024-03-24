1e1c5bdf30950e9264f2132dcd15c24941ce690f
Nuelo <akalo.emmanuel18@gmail.com>
2024-03-24 22:24:11 +0100

    feat: implement a base class

---
+++ models/base_model.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+import uuid
+from datetime import datetime
+# from __init__ import storage
+
+
+class BaseModel:
+    """
+    BaseModel for creating and managing instances
+
+    Attributes:
+        id (str): The unique identifier for the instance.
+        created_at (datetime): The date and time when the instance was created.
+        updated_at (datetime): Date and time when instance was last updated.
+    """
+    def __init__(self, *args, **kwargs):
+        """Initializes a new instance of the BaseModel class."""
+        if kwargs:
+            for attr_name, value in kwargs.items():
+                if attr_name != '__class__':
+                    setattr(self, attr_name, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary representation of the instance."""
+        obj_dict = self.__dict__.copy()
+
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+
+        return obj_dict
+
+    def save(self):
+        """Saves the instance to storage."""
+        self.updated_at = datetime.now()
+        # storage.save()
+
+    def __str__(self):
+        """Returns a string representation of the instance."""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
