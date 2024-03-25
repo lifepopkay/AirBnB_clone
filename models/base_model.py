384582b95ce5f950feb0b5ef934a28cce95d46ca
lifepopkay <popoolafolas@gmail.com>
2024-03-25 10:25:54 +0000

    correct bugs

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@
 
 import uuid
 from datetime import datetime
-from models import storage
+
 
 
 class BaseModel:
@@ -49,4 +49,27 @@ class BaseModel:
 
     def __str__(self):
         """Returns a string representation of the instance."""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        return "[{}] ({}) ({})".format(self.__class__.__name__, self.id,
+                                     self.__dict__)
+
+my_model = BaseModel()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+print(my_model.id)
+print(my_model)
+print(type(my_model.created_at))
+print("--")
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+print("--")
+my_new_model = BaseModel(**my_model_json)
+print(my_new_model.id)
+print(my_new_model)
+print(type(my_new_model.created_at))
+
+print("--")
+print(my_model is my_new_model)
