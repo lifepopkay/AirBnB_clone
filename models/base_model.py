18b8c86aa04058ed83bb6b0beb2517f1cae0c430
Nuelo <akalo.emmanuel18@gmail.com>
2024-03-25 14:46:26 +0100

    chore: update the latest state

--- models/base_model.py
+++ models/base_model.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 
+"""Import the uuid - unique identifier, the datetime module, and storage"""
+
 import uuid
 from datetime import datetime
-# from __init__ import storage
+import models
 
 
 class BaseModel:
     """
     BaseModel for creating and managing instances
+
     Attributes:
         id (str): The unique identifier for the instance.
         created_at (datetime): The date and time when the instance was created.
@@ -17,12 +20,21 @@ class BaseModel:
         """Initializes a new instance of the BaseModel class."""
         if kwargs:
             for attr_name, value in kwargs.items():
-                if attr_name != '__class__':
+                if attr_name == 'created_at' or attr_name == 'updated_at':
+                    try:
+                        fmt = "%Y-%m-%dT%H:%M:%S.%f"
+                        value = datetime.fromisoformat(kwargs[attr_name])
+                    except AttributeError:
+                        value = datetime.strptime(kwargs[attr_name], fmt)
+                if attr_name == '__class__':
+                    pass
+                else:
                     setattr(self, attr_name, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def to_dict(self):
         """Returns a dictionary representation of the instance."""
@@ -37,9 +49,9 @@ class BaseModel:
     def save(self):
         """Saves the instance to storage."""
         self.updated_at = datetime.now()
-        # storage.save()
+        models.storage.save()
 
     def __str__(self):
         """Returns a string representation of the instance."""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-        
+        return "[{}] ({}) ({})"\
+        .format(self.__class__.__name__, self.id, self.__dict__)
